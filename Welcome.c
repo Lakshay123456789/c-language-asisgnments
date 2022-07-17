20a0b6f22e9e46e094b6244c2e058d295467ba4f
Lakshay123456789 <92368897+Lakshay123456789@users.noreply.github.com>
2022-07-17 16:21:47 +0530

    Add files via upload

---
+++ Welcome.c
@@ -0,0 +1,24 @@
+main(){
+
+ //printf("Welcome\nStudents\n"); new line
+ //printf("Welcome\tStudents");   tab space
+ //printf("Welcome\rStudents\rtab"); carriage return
+ // printf("Welcome\b\bStudents\b"); BackSapce
+ // printf("Welcome\\Students");//  printf\
+//printf("\"Welcome\"Students\""); print"
+// printf("Welcome\'Students"); print'
+
+//assisgnments-1
+printf("\"MySirG\"");
+printf("%%d");
+printf("\\n");
+printf("\\");
+printf("\"students");
+
+int x = printf("lakshay");
+printf("%d",x);
+//int a=3;
+//int b=8;
+//a>=b?printf("%d",a-b):printf("%d",b-a);
+getch();
+ }
