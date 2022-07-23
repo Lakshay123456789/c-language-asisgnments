750127e3938358e82e884045b1f21d3784c0257d
Lakshay123456789 <92368897+Lakshay123456789@users.noreply.github.com>
2022-07-23 16:44:58 +0530

    Add files via upload

---
+++ assignment5.c
@@ -0,0 +1,58 @@
+int main(){
+int i,n;
+scanf("%d",&n);
+//question 1
+ /*while(n){
+ printf("MysirG\n");
+ n--;
+}
+
+//question 2;
+ for(i=1;i<=n;i++){
+  printf("%d ",i);
+}
+
+//question 3;
+
+while(n){
+    printf("%d ",n);
+    n--;
+}
+
+
+//question 4;
+for(i=1;i<=n;i++){
+    printf("%d",2*i-1);
+}
+
+ //question 5:
+     for(i=n;i>0;i--){
+    printf("%d",2*i-1);
+} */
+
+//question 6:
+     for(i=1;i<=n;i++){
+    printf("%d",2*i);
+}
+/*
+//question 7:
+     for(i=n;i>0;i--){
+    printf("%d",2*i);
+}
+
+//question 8:
+     for(i=1;i<=n;i++){
+    printf("%d ",i*i);
+}
+
+//question 9:
+     for(i=1;i<=n;i++){
+    printf("%d ",i*i*i);
+}
+
+ //question 10:
+     for(i=1;i<=10;i++){
+    printf("%d\n",n*i);
+} */
+return 0;
+}
