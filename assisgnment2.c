294cbc764f4d05959ebce673aebe9c2b2dbe4780
Lakshay123456789 <92368897+Lakshay123456789@users.noreply.github.com>
2022-07-23 16:33:34 +0530

    Add files via upload

---
+++ assisgnment2.c
@@ -0,0 +1,67 @@
+main()
+{
+   /*question 1-2 swaping
+   int a=4,b=3;
+a=a*b;
+b=a/b;
+a=a/b;
+
+a=a^b;
+b=a^b;
+a=a^b;
+
+b=a+b-(a=b);
+printf("%d%d",a,b);*/
+
+/*question 3-4
+printf("entre a vlaue\n");
+int x;
+scanf("%d",&x);
+printf("only last digit printed %d\n",x%10);
+printf("without last didit printed %d",x/10);*/
+
+/*question 5
+printf("enter the character\n");
+char c;
+scanf("%c",&c);
+printf("%d{{[[[[]]]///",c); */
+
+
+ // question 6
+ //char c='A';
+ //printf("%d",c);
+
+  // question 7
+ //  int a;
+ // float b;
+ //  char c;
+ //   double d;
+ //  printf("%d%d%d%d",sizeof(a),sizeof(b),sizeof(c),sizeof(d));
+
+
+  //question 10
+  /* int a;
+   scanf("%d",&a);
+   a=a/10;
+   a=a*10;
+   printf("%d",a); */
+
+   //question 11
+  /*int a,i;
+   scanf("%d%d",&a,&i);
+   a=a*10;
+   a+=i;
+   printf("%d",a); */
+
+   // question 12
+  /* float a;
+   scanf("%f",&a);
+   a=a/76.3;
+   printf("%.2f",a); */
+
+  //question 13
+  int a;
+  scanf("%d",&a);
+  a=a<<1;
+  printf("%d",a);
+}
